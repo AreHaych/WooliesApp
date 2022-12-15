4c9638337c4d294c4fef1e3ee3eaf617cb815501
AreHaych <101851858+AreHaych@users.noreply.github.com>
2022-12-15 15:56:59 +1100

    clean up
    Found a cleaner way to create objects for each item, using Map and forEach (40 lines less code)

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 "use strict";
 
 let itemObjectArray = []; //array of objects which represnt an item and its aisle number location
+let incorrectItemArray = []; //contains the objects of items that were incorrectly answered when prompted
 let points = 0; //number variable that increases when they correctly answer questions prompted
 
-//ITEM NAMES GROUPED BY AISLE LOCATION
-const aisle1Items = ['Bread', 'Cereal', 'Cakes?', 'Crumpets', 'Health care (vitamins, foods)', 'Honey', 'Nutella', 'High Quality Peanutbutter',
-'Specialty Jams', 'Muffins', 'Muesli and oats', 'Fruit juice'];
-const aisle0Items = ['test for 0'];
-const aisle2Items = ['Biscuits', 'Condensed milk', 'Coffee'];
-const aisle3Items = ['Batteries', 'Cards and wrap', 'Chips', 'Chocolate', 'Confectionary','Electrical goods (extension cords)',];
-const aisle4Items = ['Cordial', 'Energy drinks'];
-const aisle5Items = ['Asian food', 'Bread crumbs', 'Canned fish', 'Canned vegetables', 'Cook in sauces', 'Cooking oil', 'English foods'];
-const aisle6Items = ['Aluminium foil', 'Kitchen appliances', 'Baked beans and spaghetti', 'Bins', 'Bread mix', 'Cooking chocolate', 'Cakemix',
-'Canned fruit', 'Canned meats (ham, chicken)', 'Crockery (plates, bowls)', 'Custard', 'Cuttlery', 'Dried fruits & nuts', 
-'Desserts', 'Flour', 'Plastic Wrap/Lunch bags', 'Glassware (tumblers)'];
-const aisle7Items = ['Bandaids', 'Body lotion', 'Cosmetics (Nail polish, mascara)', 'Cotton wool balls', 'Deodorant'];
-const aisle8Items = ['Baby needs (wipes, bibs, food)', 'Bathroom towels', 'BBQ needs (Heatbeads/foil trays)', 'Brushes and brooms', 'Buttons',
-'Chlorine', 'Coathangers', 'Cotton thread', 'Firelighters', 'Garbage bags', 'Garden needs'];
-const aisle9Items = ['Air freshener', 'Bird seed', 'Bleach', 'Cat food', 'Cat litter', 'Cleaners (Kitchen, Bathroom)', 'Dishwashing detergents',
-'Disinfectants', 'Dog foog', 'Fabric care (softeners)'];
-const aisle10Items = ['Eggs'];
-const aisle11Items = ['test for 11'];
-const aisle54Items = ['test for 54'];
+//MAP KEY/VALUE PAIRS THAT REPRESENT AN AISLE NUMBER AND AN ITEM NAME
+const aislesAndItemsMap = 
+[
+    [0, 'test for 0'], [1, 'Bread'], [1, 'Cereal'], [1,'Cakes?'], [1, 'Crumpets'], [1, 'Health care (vitamins, foods)'], [1, 'Honey'], [1, 'Nutella'], [1, 'High Quality Peanutbutter'],
+    [1, 'Specialty Jams'], [1, 'Muffins'], [1, 'Muesli and oats'], [1, 'Fruit juice'], 
+    [2, 'Biscuits'], [2, 'Condensed milk'], [2, 'Coffee'], 
+    [3, 'Batteries'], [3, 'Cards and wrap'], [3, 'Chips'], [3, 'Chocolate'], [3, 'Confectionary'],[3, 'Electrical goods (extension cords)'], 
+    [4, 'Cordial'], [4, 'Energy drinks'], 
+    [5, 'Asian food'], [5, 'Bread crumbs'], [5, 'Canned fish'], [5, 'Canned vegetables'], [5, 'Cook in sauces'], [5, 'Cooking oil'], [5, 'English foods'], 
+    [6, 'Aluminium foil'], [6, 'Kitchen appliances'], [6, 'Baked beans and spaghetti'], [6, 'Bins'], [6, 'Bread mix'], [6, 'Cooking chocolate'], [6, 'Cakemix'],
+    [6, 'Canned fruit'], [6, 'Canned meats (ham, chicken)'], [6, 'Crockery (plates, bowls)'], [6, 'Custard'], [6, 'Cuttlery'], [6, 'Dried fruits & nuts'], 
+    [6, 'Desserts'], [6, 'Flour'], [6, 'Plastic Wrap/Lunch bags'], [6, 'Glassware (tumblers)'],[7,'Bandaids'], [7,'Body lotion'], [7,'Cosmetics (Nail polish, mascara)'], [7,'Cotton wool balls'], [7,'Deodorant'], 
+    [8,'Baby needs (wipes, bibs, food)'], [8,'Bathroom towels'], [8,'BBQ needs (Heatbeads/foil trays)'], [8,'Brushes and brooms'], [8,'Buttons'],
+    [8,'Chlorine'], [8,'Coathangers'], [8,'Cotton thread'], [8,'Firelighters'], [8,'Garbage bags'], [8,'Garden needs'], 
+    [9, 'Air freshener'], [9, 'Bird seed'], [9, 'Bleach'], [9, 'Cat food'], [9, 'Cat litter'], [9, 'Cleaners (Kitchen, Bathroom)'], [9, 'Dishwashing detergents'],
+    [9, 'Disinfectants'], [9, 'Dog foog'], [9, 'Fabric care (softeners)'], 
+    [10, 'Eggs'], [11, 'test for 11'], [54, 'test for 54']
+];
 
-//USES A FOREACH METHOD ON EACH ARRAY, CREATES OBJECTS BASED ON THE ITEM NAME AND AISLE, ADDS THEM TO ITEM OBJECT ARRAY
-function initialiseItemObjectArray() {
-    aisle0Items.forEach(item => itemObjectArray.push({itemName: item, itemAisle: 0}));
-    aisle1Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 1})});
-    aisle2Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 2})});
-    aisle3Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 3})});
-    aisle4Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 4})});
-    aisle5Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 5})});
-    aisle6Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 6})});
-    aisle7Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 7})});
-    aisle8Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 8})});
-    aisle9Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 9})});
-    aisle10Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 10})});
-    aisle11Items.forEach(item => { itemObjectArray.push( { itemName: item, itemAisle: 11})});
-    aisle54Items.forEach(item => {itemObjectArray.push( { itemName: item, itemAisle: 54})});
+aislesAndItemsMap.forEach(function(item) {
+    makeItemObject(item);
+})
+
+//future: is there a way to detect variable types and add into object dependign on type?
+
+/**
+ * The makeItemObject function
+ * The function takes an array and uses destructuring to use the data to create an object...
+ * ...In this instance, it is taking data from the aislesAndItemsMap
+ * The object it creates represents an item at Woolworths and it's aisle location
+ * The object will be added to the itemObjectArray
+ * @param {array} 
+ */
+function makeItemObject([aisle, name]) {
+    itemObjectArray.push(
+        {
+            itemName: name,
+            itemAisle: aisle
+        }
+    )
 }
-initialiseItemObjectArray();
 
+console.log(itemObjectArray);
 
-console.log(itemObjectArray) //error checking
 
 
 function getRandomNumber() {
@@ -50,24 +55,10 @@ function getRandomNumber() {
     return randomNumber
 }
 
-
-let incorrectItemArray = new Array(); //contains the objects of items that were incorrectly answered when prompted
-
-/**
- * The promtLoop function
- * 
- * This function starts a loop which will continuously prompt the user
- * The user input will then be checked to see if they provided a correct answer to the question prompted
- * If correct, the user will gain a point
- * If incorrect, no point will be gained
- * Regardless of right/wrong answer, the object will be deleted from the pool of questions,
- * and text will be printed to the console as feedback
- */
-function promptLoop() {
+function evaluateUserInput() {
     while(points < itemObjectArray.length) {
         let currentItemIndex = getRandomNumber();
         let currentItem = itemObjectArray[currentItemIndex];
-
         let response = window.prompt(`What aisle is ${currentItem.itemName} located in?`);
         if(response) {
             if(response == currentItem.itemAisle) {
@@ -83,10 +74,24 @@ function promptLoop() {
                 incorrectItemArray.push(itemObjectArray.splice(currentItemIndex, 1))[0];
                 console.log('Deleted ' + currentItem.itemName)
             }
-        }   
-        // console.log(incorrectItemArray)
-        console.log(itemObjectArray) 
-    }
+        }
+    }   
+}
+
+
+/**
+ * The promptLoop function
+ * This function starts a loop which will continuously prompt the user
+ * The user input will then be checked to see if they provided a correct answer to the question prompted
+ * If correct, the user will gain a point
+ * If incorrect, no point will be gained
+ * Regardless of right/wrong answer, the object will be deleted from the pool of questions,
+ * and text will be printed to the console as feedback
+ */
+function promptLoop() {
+    evaluateUserInput();
+    // console.log(incorrectItemArray)
+    console.log(itemObjectArray)    
 }
 
 promptLoop();
@@ -95,6 +100,6 @@ promptLoop();
 
 
 // //IMPORTANT!!!!
-// //eventually we want to pop off all of the item objects from the array...
+// //eventually we want to remove all of the item objects from the array...
 // //... so when the array has a length of 0, we can know when the test is finished
 
