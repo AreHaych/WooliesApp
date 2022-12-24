b4d07d9b10285820986c0fca71a7e2f663d72edc
AreHaych <101851858+AreHaych@users.noreply.github.com>
2022-12-24 15:28:14 +1100

    Display update
    Added code to show an item image next to the user input

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ let itemObjectArray = []; //array of objects which represnt an item and its aisl
 //MAP KEY/VALUE PAIRS THAT REPRESENT AN AISLE NUMBER AND AN ITEM NAME
 const aislesAndItemsMap = 
 [
-    [0, 'test for 0'], [1, 'Bread'], [1, 'Cereal'], [1,'Cakes?'], [1, 'Crumpets'], [1, 'Health care (vitamins, foods)'], [1, 'Honey'], [1, 'Nutella'], [1, 'High Quality Peanutbutter'],[1, 'Specialty Jams'], [1, 'Muffins'], [1, 'Muesli and oats'], 
-    [2, 'Biscuits'], [2, 'Condensed milk'], [2, 'Coffee'], 
-    [3, 'Batteries'], [3, 'Cards and wrap'], [3, 'Chips'], [3, 'Chocolate'], [3, 'Confectionary'],[3, 'Electrical goods (extension cords)'], 
-    [4, 'Cordial'], [4, 'Energy drinks'], 
-    [5, 'Asian food'], [5, 'Bread crumbs'], [5, 'Canned fish'], [5, 'Canned vegetables'], [5, 'Cook in sauces'], [5, 'Cooking oil'], [5, 'English foods'], 
-    [6, 'Aluminium foil'], [6, 'Kitchen appliances'], [6, 'Baked beans and spaghetti'], [6, 'Bins'], [6, 'Bread mix'], [6, 'Cooking chocolate'], [6, 'Cakemix'],
-    [6, 'Canned fruit'], [6, 'Canned meats (ham, chicken)'], [6, 'Crockery (plates, bowls)'], [6, 'Custard'], [6, 'Cuttlery'], [6, 'Dried fruits & nuts'], 
-    [6, 'Desserts'], [6, 'Flour'], [6, 'Plastic Wrap/Lunch bags'], [6, 'Glassware (tumblers)'],[7,'Bandaids'], [7,'Body lotion'], [7,'Cosmetics (Nail polish, mascara)'], [7,'Cotton wool balls'], [7,'Deodorant'], 
-    [8,'Baby needs (wipes, bibs, food)'], [8,'Bathroom towels'], [8,'BBQ needs (Heatbeads/foil trays)'], [8,'Brushes and brooms'], [8,'Buttons'],
-    [8,'Chlorine'], [8,'Coathangers'], [8,'Cotton thread'], [8,'Firelighters'], [8,'Garbage bags'], [8,'Garden needs'], 
-    [9, 'Air freshener'], [9, 'Bird seed'], [9, 'Bleach'], [9, 'Cat food'], [9, 'Cat litter'], [9, 'Cleaners (Kitchen, Bathroom)'], [9, 'Dishwashing detergents'],
-    [9, 'Disinfectants'], [9, 'Dog foog'], [9, 'Fabric care (softeners)'], 
-    [10, 'Eggs'], [11, 'test for 11'], [54, 'test for 54']
+    // [0, 'test for 0'], 
+    [1, 'Bread', 'images/bread.jpg']
+    //  [1, 'Cereal'], [1,'Cakes?'], [1, 'Crumpets'], [1, 'Health care (vitamins, foods)'], [1, 'Honey'], [1, 'Nutella'], [1, 'High Quality Peanutbutter'],[1, 'Specialty Jams'], [1, 'Muffins'], [1, 'Muesli and oats'], 
+    // [2, 'Biscuits'], [2, 'Condensed milk'], [2, 'Coffee'], 
+    // [3, 'Batteries'], [3, 'Cards and wrap'], [3, 'Chips'], [3, 'Chocolate'], [3, 'Confectionary'],[3, 'Electrical goods (extension cords)'], 
+    // [4, 'Cordial'], [4, 'Energy drinks'], 
+    // [5, 'Asian food'], [5, 'Bread crumbs'], [5, 'Canned fish'], [5, 'Canned vegetables'], [5, 'Cook in sauces'], [5, 'Cooking oil'], [5, 'English foods'], 
+    // [6, 'Aluminium foil'], [6, 'Kitchen appliances'], [6, 'Baked beans and spaghetti'], [6, 'Bins'], [6, 'Bread mix'], [6, 'Cooking chocolate'], [6, 'Cakemix'],
+    // [6, 'Canned fruit'], [6, 'Canned meats (ham, chicken)'], [6, 'Crockery (plates, bowls)'], [6, 'Custard'], [6, 'Cuttlery'], [6, 'Dried fruits & nuts'], 
+    // [6, 'Desserts'], [6, 'Flour'], [6, 'Plastic Wrap/Lunch bags'], [6, 'Glassware (tumblers)'],[7,'Bandaids'], [7,'Body lotion'], [7,'Cosmetics (Nail polish, mascara)'], [7,'Cotton wool balls'], [7,'Deodorant'], 
+    // [8,'Baby needs (wipes, bibs, food)'], [8,'Bathroom towels'], [8,'BBQ needs (Heatbeads/foil trays)'], [8,'Brushes and brooms'], [8,'Buttons'],
+    // [8,'Chlorine'], [8,'Coathangers'], [8,'Cotton thread'], [8,'Firelighters'], [8,'Garbage bags'], [8,'Garden needs'], 
+    // [9, 'Air freshener'], [9, 'Bird seed'], [9, 'Bleach'], [9, 'Cat food'], [9, 'Cat litter'], [9, 'Cleaners (Kitchen, Bathroom)'], [9, 'Dishwashing detergents'],
+    // [9, 'Disinfectants'], [9, 'Dog foog'], [9, 'Fabric care (softeners)'], 
+    // [10, 'Eggs'], [11, 'test for 11'], [54, 'test for 54']
 ];
 
 aislesAndItemsMap.forEach(function(item) {
@@ -34,12 +36,12 @@ aislesAndItemsMap.forEach(function(item) {
  * The object will be added to the itemObjectArray
  * @param {array} 
  */
-function makeItemObject([aisle, name]) {
+function makeItemObject([aisle, name, image]) {
     itemObjectArray.push(
         {
             itemName: name,
-            itemAisle: aisle
-        }
+            itemAisle: aisle,
+            itemImage: image        }
     )
 }
 
@@ -50,7 +52,7 @@ function makeItemObject([aisle, name]) {
  * @returns A random number between 0 and length of the itemObjectArray
  */
 function getRandomNumber() {
-    return Math.floor(Math.random() * itemObjectArray.length) + 1; //issue   
+    return Math.floor(Math.random() * itemObjectArray.length); //issue   
 }
  
 
@@ -58,19 +60,31 @@ let incorrectItemArray = []; //contains the objects of items that were incorrect
 let points = 0; //number variable that increases when they correctly answer questions prompted
 let currentItemIndex = getRandomNumber();
 let currentItem = itemObjectArray[currentItemIndex]; //get a random object from itemObjectArray
+console.log(itemObjectArray)
 
 
 //DOM elements that will be used
 let pointsEle = document.getElementById('points').innerHTML = `Points: ${points}`;
-let currentItemMessageEle = document.getElementById('current-item-message').innerHTML = `What aisle is ${currentItem.itemName} located?`;
+let currentItemMessageEle = document.getElementById('current-item-message').innerHTML = `What aisle is ${currentItem.itemName} located in?`;
 let userInput = document.getElementById('user-input');
 let userFeedbackEle = document.getElementById('user-feedback');
+let currentItemImage = document.getElementById('current-item-image').src = currentItem.itemImage;
+
+
+//event listener for button when user presses enter
+let textArea = document.getElementById('user-input').addEventListener('keyup', function(event) {
+    if(event.key === 'Enter') {
+        userInputCheck();
+    }
+});
+
 
 function userInputCheck() {
     if(userInput.value) {
         if(userInput.value == currentItem.itemAisle) {
             points++;
             userFeedbackEle.innerText = 'Correct';
+            userFeedbackEle.style = 'color: green';
             itemObjectArray.splice(currentItemIndex, 1)[0];
             resetValues();
         } else {
@@ -88,7 +102,7 @@ function userInputCheck() {
 function resetValues() {
     currentItemIndex = getRandomNumber();
     currentItem = itemObjectArray[currentItemIndex];
-    currentItemMessageEle = document.getElementById('current-item-message').innerHTML = `What aisle is ${currentItem.itemName} located?`;
+    currentItemMessageEle = document.getElementById('current-item-message').innerHTML = `What aisle is ${currentItem.itemName} located in?`;
     pointsEle = document.getElementById('points').innerHTML = `Points: ${points}`;
     userInput.value = '';
 }
